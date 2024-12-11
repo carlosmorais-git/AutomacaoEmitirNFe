5f99f4c6bd91d152bb56c280d9b69857ec31aeb2
carlos silvano morais <127549934+carlosmorais-git@users.noreply.github.com>
2024-12-11 17:38:19 -0300

    Initial commit

---
+++ credenciais.py
@@ -0,0 +1,5 @@
+# Estou armazenando os dados de login separadamente como uma boa prática, 
+# pois, nesta tela de login fictícia, qualquer credencial inserida funcionará.
+
+login = 'Carlos Morais'
+senha = '*********'
